fa392c39f0a6340e21d03e9fe1956db2cc6a08f2
anastasiya <nastya.panysh@gmail.com>
2022-10-30 22:36:46 +0300

    add

--- app.js
+++ app.js
@@ -1,16 +1,83 @@
 class Calculator {
-    constructor(){
-        this.clear()
-        this.calc()
-    }
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement
+    this.currentOperandTextElement = currentOperandTextElement
+    this.clear()
+  }
 
-    clear() {
-        document.querySelector('.display').value = "";
+  clear() {
+    this.currentOperand = ''
+    this.previousOperand = ''
+    this.operation = ''
+  }
+
+  delete() {
+    this.currentOperand = this.currentOperand.toString().slice(0, -1)
+  }
+
+  appendNumber(number) {}
+
+  chooseOperation(operation) {}
+
+  compute() {
+    let computation
+    const prev = parseFloat(this.previousOperand)
+    const current = parseFloat(this.currentOperand)
+    if (isNaN(prev) || isNaN(current)) return
+    switch (this.operation) {
+      case '+':
+        computation = prev + current
+        break
+      case '-':
+        computation = prev - current
+        break
+      case '*':
+        computation = prev * current
+        break
+      case '÷':
+        computation = prev / current
+        break
+      default:
+        return
     }
-    calc() {
-        document.querySelector('.display').value = eval( document.querySelector('.display').value);
-    }
-    press(x) {
-        document.querySelector('.display').value += x;
-    }
+    this.currentOperand = computation
+
+    this.previousOperand = ''
+  }
+
+  updateDisplay() {}
 }
+
+
+const numberButtons = document.querySelector('.data-number')
+const operationButtons = document.querySelector('.data-operation')
+const equalsButton = document.querySelector('.data-equals')
+const allClearButton = document.querySelector('.data-delete')
+const previousOperandTextElement = document.querySelector('.previous-operand')
+const currentOperandTextElement = document.querySelector('.current-operand')
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText)
+    calculator.updateDisplay()
+  })
+})
+
+operationButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.chooseOperation(button.innerText)
+    calculator.updateDisplay()
+  })
+})
+
+equalsButton.addEventListener('click', button => {
+  calculator.compute()
+  calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click', button => {
+  calculator.clear()
+  calculator.updateDisplay()
+})
