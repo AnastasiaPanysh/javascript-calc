c706e6becded0183ea920b423afb0972b0d34b8e
anastasiya <nastya.panysh@gmail.com>
2022-11-02 23:10:18 +0300

    add

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+const rectangle= document.querySelector('.rectangle')
+
+document.querySelector('.theme p').addEventListener('mouseover', () => {
+  rectangle.hidden = false;
+})
+document.querySelector('.theme p').addEventListener('mouseout', () => {
+  rectangle.hidden = true;
+})
+
+
+
+
 const res = document.querySelector('.result');
 const history = document.querySelector('.history');
 const calc = document.querySelector('.calc-wrapper');
@@ -14,10 +26,12 @@ calc.addEventListener('click', function (event) {
       break;
     case 'C':
       res.innerHTML = ''
+      history.innerHTML = ''
+
       break;
     case '±':
       if (res.textContent) {
-        resultStrintMath =`${+res.textContent * -1}`
+        resultStrintMath = `${+res.textContent * -1}`
         res.innerHTML = resultStrintMath
       } else {
         history.innerHTML = `${+history.textContent * -1}`
@@ -32,4 +46,3 @@ calc.addEventListener('click', function (event) {
       res.textContent = resultStrintMath;
   }
 })
-
